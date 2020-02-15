5b812afab77e5a1d13a9aedf6914b86853ab969e
Alpha Olomi <alphaolomi@gmail.com>
2020-02-15 11:37:05 +0300

    Update file-io.c

--- 11-std-lib/file-io.c
+++ 11-std-lib/file-io.c
@@ -1,15 +1,17 @@
 // file-io.c
 #include <stdio.h>
 
-int main( int argc, char ** argv ) {
-	const char * fn = "test.file";			// file name
-	const static int maxString = 1024;	// read buffer size
-	const char * str = "This is a literal C-string.\n";
+int main(int argc, char **argv)
+{
+	const char *fn = "test.txt";	  // file name
+	const static int maxString = 1024; // read buffer size
+	const char *str = "This is a literal C-string.\n";
 
 	// create/write the file
 	printf("writing file\n");
-	FILE * fw = fopen(fn, "w");
-	for(int i = 0; i < 5; i++) {
+	FILE *fw = fopen(fn, "w");
+	for (int i = 0; i < 5; i++)
+	{
 		fputs(str, fw);
 	}
 	fclose(fw);
@@ -18,12 +20,14 @@ int main( int argc, char ** argv ) {
 	// read the file
 	printf("reading file\n");
 	char buf[maxString];
-	FILE * fr = fopen(fn, "r");
-	char * rc;
-	while(( rc = fgets(buf, maxString, fr) )) {
+	FILE *fr = fopen(fn, "r");
+	char *rc;
+	while ((rc = fgets(buf, maxString, fr)))
+	{
 		printf(buf);
 	}
 	fclose(fr);
+	
 	printf("done.\n");
 
 	return 0;
